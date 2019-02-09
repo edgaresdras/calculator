d37aa8f1b6d3bd0a81080056750ec426f64ff98b
esdrasgreco <esdras_greco@yahoo.com>
2019-02-08 20:52:45 -0600

    Calculadora con suma, resta, division y multiplicacion

---
+++ index.js
@@ -0,0 +1,7 @@
+const math = require('./math');
+
+console.log(math.suma(10, 20));
+console.log(math.resta(10, 20));
+console.log(math.division(10, 20));
+console.log(math.multiplicacion(10, 20));
+
